39f73ab4a69f4f75dd3f15db6936a11f3d928b12
kafriyiegyamfi <kafriyiegyamfi@gmail.com>
2020-11-17 12:19:51 +0000

    added validation logic

--- src/main/java/com/trading_engine/order_validation/resources/OrderValidatorResource.java
+++ src/main/java/com/trading_engine/order_validation/resources/OrderValidatorResource.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+import static java.lang.Math.abs;
 
 
 @RestController
@@ -34,14 +34,45 @@ public class OrderValidatorResource {
 
 
         return orderItems.stream().map(orderItem ->{
-            Stockdata stockdata = restTemplate.getForObject("http://localhost:8081/md/"+orderItem.getProduct()  , Stockdata.class);
-            if (orderItem.getSide()=="BUY"&& stockdata.getBuy_Limit()>=orderItem.getQuantity()){
-            return new ValidatedOrderItem(orderItem, "Validated");
+            Stockdata stockdata1 = restTemplate.getForObject("https://exchange.matraining.com/md/"+orderItem.getProduct()  , Stockdata.class);
+            Stockdata stockdata2 = restTemplate.getForObject("https://exchange2.matraining.com/md/"+orderItem.getProduct()  , Stockdata.class);
+            Stockdata AvData =
+                    new Stockdata(
+                            (stockdata1.getBid_Price()+stockdata2.getBid_Price())/2,
+                            (stockdata1.getAsk_Price()+stockdata2.getAsk_Price())/2,
+                            stockdata2.getBuy_Limit(),
+                            orderItem.getProduct(),
+                            stockdata1.getSell_Limit(),
+                            Math.max(stockdata1.getLast_Traded_Price(), stockdata2.getLast_Traded_Price()),
+                            Math.max(stockdata1.getMax_Price_Shift(), stockdata2.getMax_Price_Shift())
+                    );
+
+
+
+            if (orderItem.getSide()=="BUY"){
+                    if (AvData.getBuy_Limit()>=orderItem.getQuantity()){
+                return new ValidatedOrderItem(orderItem, "Validated");
+                }
+                if (  AvData.getMax_Price_Shift()<(abs(orderItem.getPrice()-AvData.getSell_Limit()))
+                        || ((orderItem.getPrice()/AvData.getBid_Price())*100 < 120)){
+                    return new ValidatedOrderItem(orderItem, "Validated");
+                }
+                return new ValidatedOrderItem(orderItem, "Not Valid");
             }
-            else if(orderItem.getSide()=="SELL" && stockdata.getSell_Limit()>=orderItem.getQuantity()){
-                return new ValidatedOrderItem(orderItem, "Validated");}
+            else if(orderItem.getSide()=="SELL"){
+                if(AvData.getSell_Limit()>=orderItem.getQuantity()){
+                return new ValidatedOrderItem(orderItem, "Validated");
+                }
+                if (
+                        AvData.getMax_Price_Shift()<(abs(orderItem.getPrice()-AvData.getAsk_Price()))
+                        || (orderItem.getPrice()/AvData.getAsk_Price()*100)> AvData.getAsk_Price()*1.2 )
+                {
+                    return new ValidatedOrderItem(orderItem, "Validated");
+                }
+              return new ValidatedOrderItem(orderItem, "Not Valid");
+            }
             else
-                return new ValidatedOrderItem(orderItem, " Not Valid");
+                return new ValidatedOrderItem(orderItem, "Not Valid");
         }).collect(Collectors.toList());
 
     }
